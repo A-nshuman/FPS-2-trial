dbc094558e8f8603f61f60fd03ee5095ebccede2
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-10-11 19:23:47 +0300

    Add files via upload

---
+++ Assets/Scripts/Helper Script/TagHolder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    public class Axis
+    {
+        public const string HORIZONTAL = "Horizontal";
+        public const string VERTICAL = "Vrtical";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController character_Controller;
+
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump = 10f;
+        private float vertical_Velocity;
+
+    void Awake() { 
+         character_Controller = GetComponent<CharacterController>();
+        }
+
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+        void MoveThePlayer() { 
+
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f,
+                                     Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        character_Controller.Move(move_Direction);
+
+        }
+    }
+
